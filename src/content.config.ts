086680690b7293b28b829a1888fc328f8f59155c
Iafcy622 <frankiecheung512@gmail.com>
2025-06-02 12:52:25 +0800

    Added new photos

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,50 +1,52 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-import { glob, file } from 'astro/loaders';
+import { glob, file } from "astro/loaders";
 
 const images = defineCollection({
-    loader: file('src/content/data/images.json'),
-    schema: ({ image }) => z.object({
-        id: z.number(),
-        src: image(),
-        alt: z.string(),
-    })
+    loader: file("src/content/data/images.json"),
+    schema: ({ image }) =>
+        z.object({
+            id: z.string(),
+            src: image(),
+            alt: z.string(),
+        }),
 });
 
 const research = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/research" }),
+    loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/research" }),
     schema: z.object({
         title: z.string(),
         location: z.string(),
         duration: z.string(),
-    })
+    }),
 });
 
 const education = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/education" }),
+    loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/education" }),
     schema: z.object({
         title: z.string(),
         location: z.string(),
         duration: z.string(),
-    })
+    }),
 });
 
 const experience = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/experience" }),
-    schema: ({ image }) => z.object({
-        title: z.string(),
-        location: z.string(),
-        duration: z.string(),
-        image: image(),
-    })
+    loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/experience" }),
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            location: z.string(),
+            duration: z.string(),
+            image: image(),
+        }),
 });
 
 const awards = defineCollection({
-    loader: file('src/content/data/awards.json'),
+    loader: file("src/content/data/awards.json"),
     schema: z.object({
         id: z.number(),
         name: z.string(),
-    })
+    }),
 });
 
 export const collections = { images, research, education, experience, awards };
